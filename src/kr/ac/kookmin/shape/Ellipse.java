510d67bd57fdf2117a4d5d0fc9e460148665c5a8
inkyu <in1004kyu@gmail.com>
2015-09-24 11:36:43 +0900

    homework

---
+++ src/kr/ac/kookmin/shape/Ellipse.java
@@ -0,0 +1,25 @@
+package kr.ac.kookmin.shape;
+
+public class Ellipse extends Shape{
+	private int w;
+	private int h;
+	
+	public Ellipse( Point center, int w, int h) {
+		super( center );
+		this.w = w;
+		this.h = h;
+	}
+	public Rectangle getBounds(){
+		return new Rectangle(center, w*2, h*2);
+	}
+	public int getSemiMajorAxis(){ return w; }
+	public int getSemiMinorAxis(){ return h; }
+	@Override
+	public void draw(Graphics g) {
+		g.draw(this);
+	}
+	@Override
+	public String toString() {
+		return "Ellipse";
+	}
+}
---
+++ src/kr/ac/kookmin/shape/Rectangle.java
@@ -0,0 +1,26 @@
+package kr.ac.kookmin.shape;
+
+public class Rectangle extends Shape{
+	private int h;
+	private int w;
+	
+	public Rectangle(Point center, int w, int h) {
+		super(center);
+		this.w = w;
+		this.h = h;
+	}
+	public Rectangle getBounds() {
+		return this;
+	}
+	public int getHeight(){ return h; }
+	public int getWidth(){ return w; }
+	
+	@Override
+	public void draw(Graphics g) {
+		g.draw(this);
+	}
+	@Override
+	public String toString() {
+		return "Rectangle";
+	}
+}
